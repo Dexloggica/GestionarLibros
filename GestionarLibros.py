25e062a6cba1c57ed95702d557f953bf97319f22
Dex <42595658+Dexloggica@users.noreply.github.com>
2020-02-09 20:01:06 -0300

    Update GestionarLibros.py
    Se puede registrar el libro y establecer conexion con la base de datos del sistema de gestion escolar...

--- GestionarLibros.py
+++ GestionarLibros.py
@@ -2,6 +2,14 @@
 
 #author:Dex Loggica
 
+#from pymysql import *
+####################################BASE DE DATOS
+#me conecto a la base de datos
+import pymysql
+
+
+
+####################################
 from tkinter import *
 from math import *
 
@@ -21,15 +29,71 @@ ventana.configure(background="white")
 color_boton="gray99"
 ancho_boton=10
 alto_boton=1
-entrada=StringVar()
+title=StringVar()
+#title="hola"
+cant=IntVar()
+#cant=0
+
 #botones de la PRIMER fila
 #el metodo grid es para situarlo en la pantalla
 Texto0=Label(text="Libro: ",font=("Chiller",15),bg="white").grid(row=0,column=0,pady=10)
-Titulo=Entry(ventana,textvariable=entrada).grid(row=0,column=1,pady=10)
+Entry0=Entry(ventana,textvariable=title).grid(row=0,column=1,pady=10)
 Texto1=Label(text="Cantidad de paginas: ",font=("Chiller",15),bg="white").grid(row=1,column=0,pady=10)
-CantPaginas=Entry(ventana,textvariable=entrada).grid(row=1,column=1,pady=10)
+Entry1=Entry(ventana,textvariable=cant).grid(row=1,column=1,pady=10)
 
-Registrar= Button(ventana, text="Registrar",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:click(0)).grid(row=2,column=0,pady=10)
-Boton1= Button(ventana, text="Mostrar",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:click(1)).grid(row=2,column=1,pady=10)
-#linea de codigo que va al final si o si
-ventana.mainloop()
+def conectar():
+    connection = pymysql.connect(host='localhost',user='root',password='', db='20181020_version1')
+    return connection
+
+def insertar():
+    print(title.get())
+    print(cant.get())
+    #print(title1)
+   # print(cant1)
+    #print(Entry0)
+    #print(Entry1)
+    titulo=title.get()
+    cantidad=cant.get()
+    
+    print(titulo)
+    print(cantidad)
+    #abro la conexion
+    connect = conectar()
+    #creo el puntero
+    cursor=connect.cursor()
+    #realizar consulta sql
+    sql="INSERT INTO libro (Titulo,Numero,Pais_idPais,Editorial_idEditorial,Estado) VALUES ('%s', '%d', '%d', '%d', '%d')" % (titulo, cantidad,1,1,1)
+    try:
+        print("try")
+        cursor.execute(sql)
+        #cerrar puntero
+        connect.commit()
+    except:
+        print("except")
+        #en el caso de error
+        connect.rollback()
+        
+    cursor.close()
+    connect.close()
+    
+def mostrar():
+    connect = conectar()
+    cursor=connect.cursor()
+    cursor.execute("SELECT idLibro, Titulo FROM libro")
+    
+    for Titulo, Numero in cursor.fetchall():
+        print("{0} {1}".format(Titulo, Numero))
+    
+    cursor.close()
+    connect.commit()
+    connect.close()
+    
+    
+def main():
+    Registrar= Button(ventana, text="Registrar",bg=color_boton,width=ancho_boton,height=alto_boton,command= insertar).grid(row=2,column=0,pady=10)
+    Mostrar= Button(ventana, text="Mostrar",bg=color_boton,width=ancho_boton,height=alto_boton,command= mostrar).grid(row=2,column=1,pady=10)
+    #linea de codigo que va al final si o si
+    ventana.mainloop()
+
+if __name__ == "__main__":
+    main()
